ebcff8b0181e4d515307c86f17675c407fc314e8
Calle1122 <carl@lindstedt.net>
2022-09-26 19:47:01 +0200

    More UI Tweaks
    Fixed the lobby UI

---
+++ Assets/Scripts/UI/LobbyScript.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyScript : MonoBehaviour
+{
+    [SerializeField] private GameObject activeP3, activeP4;
+    [SerializeField] private GameObject inActiveP3, inActiveP4;
+    [SerializeField] private GameObject nameP3, nameP4;
+
+    public void TwoActive()
+    {
+        activeP3.SetActive(false);
+        activeP4.SetActive(false);
+        
+        inActiveP3.SetActive(true);
+        inActiveP4.SetActive(true);
+        
+        nameP3.SetActive(false);
+        nameP4.SetActive(false);
+    }
+    
+    public void ThreeActive()
+    {
+        activeP3.SetActive(true);
+        activeP4.SetActive(false);
+        
+        inActiveP3.SetActive(false);
+        inActiveP4.SetActive(true);
+        
+        nameP3.SetActive(true);
+        nameP4.SetActive(false);
+    }
+
+    public void FourActive()
+    {
+        activeP3.SetActive(true);
+        activeP4.SetActive(true);
+        
+        inActiveP3.SetActive(false);
+        inActiveP4.SetActive(false);
+        
+        nameP3.SetActive(true);
+        nameP4.SetActive(true);
+    }
+
+}
